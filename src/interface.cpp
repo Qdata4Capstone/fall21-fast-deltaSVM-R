90a2c2735c123e02c714ff4c9122cc9b5af10853
k-ivey <kivey1@gmail.com>
2021-03-23 15:39:32 -0400

    Add skeleton project structure

---
+++ src/interface.cpp
@@ -0,0 +1,22 @@
+
+#include <Rcpp.h>
+#include "fastsk.hpp"
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+void compute_kernel() {
+
+    return;
+}
+
+// [[Rcpp::export]]
+void fit() {
+
+    return;
+}
+
+// [[Rcpp::export]]
+double score() {
+
+    return 0;
+}
